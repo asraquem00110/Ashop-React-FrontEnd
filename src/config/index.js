ae4a3b9c5aadddf74610b4864e1592488706562a
Alvin S. Raquem <41246806+asraquem00110@users.noreply.github.com>
2020-10-08 15:23:57 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const backendapi = "http://ashopping.herokuapp.com/api/"
-export const imgUrl = "http://ashopping.herokuapp.com/upload/"
+export const backendapi = "https://ashopping.herokuapp.com/api/"
+export const imgUrl = "https://ashopping.herokuapp.com/upload/"
